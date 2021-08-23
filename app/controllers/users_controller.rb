a3b95d4cd28f29134c8729fb01946d54c76b2661
n.shmelyov <shmelev507@gmail.com>
2021-08-23 16:12:12 +0200

    pagination on home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-        @posts = Post.all
+        @posts = Post.paginate(page: params[:page], per_page: 3)
+        # @paginate_check = false
         return @posts, @user
     end  
 end
